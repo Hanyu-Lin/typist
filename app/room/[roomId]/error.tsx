de487666de639c4d154da0845dc1cb5557c168f3
Hanyu-Lin <hanyulin02@gmail.com>
2024-07-09 07:08:48 +0200

    refactor: Update error page layout and add error images

--- app/room/[roomId]/error.tsx
+++ app/room/[roomId]/error.tsx
@@ -1,27 +1,39 @@
-'use client'; // Error components must be Client Components
-import { Button } from '@/components/ui/button';
+'use client';
+
+import Image from 'next/image';
 import Link from 'next/link';
-import { useEffect } from 'react';
 
-export default function Error({
-  error,
-}: {
-  error: Error & { digest?: string };
-  reset: () => void;
-}) {
-  useEffect(() => {
-    console.error(error);
-  }, [error]);
+import { Button } from '@/components/ui/button';
 
+const Error = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-6 h-[50vh]">
-      <h2 className="font-bold text-3xl text-red-500">404: Room not found </h2>
-      <p className="text-wrap">
-        The room you are looking for does not exist or has been removed.
+    <div className="h-full grid gap-4 place-items-center space-y-4">
+      <Image
+        src="/error.png"
+        height="300"
+        width="300"
+        alt="Error"
+        className="dark:hidden"
+      />
+      <Image
+        src="/error-dark.png"
+        height="300"
+        width="300"
+        alt="Error"
+        className="hidden dark:block"
+      />
+      <h2 className="text-3xl lg:text-5xl font-black text-orange-400">
+        404: Room not found
+      </h2>
+
+      <p className="text-center w-72 font-bold tracking-tight">
+        Uh-oh! The room you are looking for does not exist or has been removed.
       </p>
-      <Button className="mt-8">
-        <Link href="/">Go back to the homepage</Link>
+      <Button asChild>
+        <Link href="/">Go back</Link>
       </Button>
     </div>
   );
-}
+};
+
+export default Error;
